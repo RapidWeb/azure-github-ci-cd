8abd775001f89891ab0fc9aa92400cc005c6ed60
DongyueWang <dongyue.wang@gmail.com>
2017-07-09 13:15:20 +0200

    release 1.04

--- staging-deploy.ps1
+++ staging-deploy.ps1
@@ -3,7 +3,7 @@ $repo = 'https://github.com/RapidWeb/azure-github-ci-cd.git'
 $resourceGroup = 'ms-azure-github-dong'
 
 az webapp deployment source delete --name ms-azure-github-dong --resource-group ms-azure-github-dong --slot staging
-az webapp deployment source config --name ms-azure-github-dong --repo-url $repo --resource-group $resourceGroup --branch $branch --slot staging
+az webapp deployment source config --name ms-azure-github-dong --repo-url $repo --resource-group $resourceGroup --branch $branch --slot stagingwer
 
 # example 
 # .\staging-deploy.ps1 "release/1.0.2"
